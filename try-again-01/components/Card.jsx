311b02a24d7f467fa99d831c8deb4e0f4cfd37ee
prince05-ku <gwprince141@gmail.com>
2025-11-28 11:44:42 +0530

    learn a component base structure

---
+++ try-again-01/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <h1>prince</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, obcaecati!</p>
+    </div>
+  )
+}
+
+export default Card
